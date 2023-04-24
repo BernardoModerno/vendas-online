171aa8b1bd9892fe6e3b5d41a6d6f215d9aa607c
BernardoModerno <bernardo.moderno@gmail.com>
2023-04-24 11:14:00 -0300

    arquivo App

---
+++ mobile/src/App.tsx
@@ -0,0 +1,13 @@
+import { SafeAreaView, Text } from "react-native"
+
+const App = () => {
+
+    return (
+        <SafeAreaView>
+            <Text>Testanto ...</Text>
+        </SafeAreaView>
+
+    )
+};
+
+export default App;
