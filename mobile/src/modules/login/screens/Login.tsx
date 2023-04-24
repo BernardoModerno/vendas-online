d2a17f9321c7996f6bb30e133cfb76ed91bd307e
BernardoModerno <bernardo.moderno@gmail.com>
2023-04-24 16:34:00 -0300

    create shared button

--- mobile/src/modules/login/screens/Login.tsx
+++ mobile/src/modules/login/screens/Login.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
+import Button from '../../../shared/components/button/Button';
 import Input from '../../../shared/components/input/Input';
 import { ContainerLogin } from '../styles/login.style';
 
 // import { Container } from './styles';
 
 const Login = () => {
+  const handleOnPress = () => {
+    console.log('clicou');
+  };
   return (
     <View>
       <ContainerLogin>
         <Text>lkj</Text>
         <Input />
+        <Button margin="16px" title="ENTRAR" onPress={handleOnPress} />
       </ContainerLogin>
     </View>
   );
